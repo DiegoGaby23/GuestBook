7abed8cb2a71a742ed2ac341e8924107ba2a9b03
DiegoGaby <gabyperezmota@icloud.com>
2018-03-18 01:25:40 +0100

    Fonction fléchée

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,33 @@
 module.exports = (app) => {
     let entries = [];
     app.locals.entries = entries;
-// Route
-app.get("/", function (req, res) {
-    res.render("index", {
-        title: "Bienvenue à mon site en NodeJs ! "
+    // Route
+    app.get("/",  (req, res) => {
+        res.render("index", {
+            title: "Bienvenue à mon site en NodeJs ! "
+        });
     });
-});
 
-app.get("/new-entry", function (req, res) {
-    res.render("new-entry", {
-        title: "new-entry"
+    app.get("/new-entry", function (req, res) {
+        res.render("new-entry", {
+            title: "new-entry"
+        });
     });
-});
 
-app.post("/new-entry",  (req, res) => {
-    if (!req.body.title || !req.body.body) {
-        res.send(404).send("Error page not found");
-    }
+    app.post("/new-entry", (req, res) => {
+        if (!req.body.title || !req.body.body) {
+            res.send(404).send("Error page not found");
+        }
 
 
-let newEntry = {
-    title: req.body.title,
-    content: req.body.body,
-    pubished: new Date()
-};
-entries.push(newEntry);
-res.redirect("/");
-});
+        let newEntry = {
+            title: req.body.title,
+            content: req.body.body,
+            pubished: new Date()
+        };
+        entries.push(newEntry);
+        res.redirect("/");
+    });
 }
 
 
